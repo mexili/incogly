36450ff130092b2a03ff52efe35de1efdc80e740
Aniket Kumar <aniketkumar049@gmail.com>
2020-07-15 21:48:33 +0530

    ssl added for localhost

--- server.js
+++ server.js
@@ -9,14 +9,15 @@ const compression = require('compression');
 
 const app = express(),
   options = { 
-    key: fs.readFileSync(__dirname + '/rtc-video-room-key.pem'),
-    cert: fs.readFileSync(__dirname + '/rtc-video-room-cert.pem')
+    key: fs.readFileSync(__dirname + '/key.pem'),
+    cert: fs.readFileSync(__dirname + '/cert.pem')
   },
   port = process.env.PORT || 3000,
   server = process.env.NODE_ENV === 'production' ?
     http.createServer(app).listen(port) :
-    https.createServer(options, app).listen(port),
+    https.createServer(options, app).listen(port, () => console.log('running')),
   io = sio(server);
+  
 // compress all requests
 app.use(compression());
 app.use(express.static(path.join(__dirname, 'dist')));
