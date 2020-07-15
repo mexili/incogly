e846ed56003de298dbc2aaf5c65582f01c3da70b
Aniket Kumar <aniketkumar049@gmail.com>
2020-07-15 13:47:35 +0530

    webpack config changed

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,7 @@ const path = require("path");
 module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 9000
+    compress: true
   },
   module: {
     rules: [
