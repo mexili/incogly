820a4cdfb0f1219da977a84c793cef877acf8c86
Nishant Jawla <40868641+NishantJawla@users.noreply.github.com>
2021-02-16 00:44:19 -0800

    Convert CSS to Scss files
    Added node-sass@4.14.1 package to run SCSS. [The latest version of node-sass is not compatible]. changed files from CSS to SCSS and changed all the SCSS file address used

--- src/Video.js
+++ src/Video.js
@@ -18,7 +18,7 @@ import 'antd/dist/antd.css'
 import { Row } from 'reactstrap'
 import Modal from 'react-bootstrap/Modal'
 import 'bootstrap/dist/css/bootstrap.css'
-import "./Video.css"
+import "./Video.scss"
 
 const server_url = process.env.NODE_ENV === 'production' ? '' : "http://localhost:5000"
 
