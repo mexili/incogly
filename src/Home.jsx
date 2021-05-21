c373fb9e792274abe085523f2b0410520ee244c3
Sanskar Jethi <sansyrox@gmail.com>
2021-05-21 14:44:53 +0530

    Convert home component to functional

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,47 +1,67 @@
-import React, { Component } from 'react';
-import { Input, Button, IconButton } from '@material-ui/core';
-import GitHubIcon from '@material-ui/icons/GitHub';
-import "./Home.scss"
+import React, { Component, useState } from "react";
 
-class Home extends Component {
-  	constructor (props) {
-		super(props)
-		this.state = {
-			url: ''
-		}
-	}
+import { Input, Button, IconButton } from "@material-ui/core";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import "./Home.scss";
 
-	handleChange = (e) => this.setState({ url: e.target.value })
+function Home() {
+	const [url, setUrl] = useState("");
 
-	join = () => {
-		if (this.state.url !== "") {
-			var url = this.state.url.split("/")
-			window.location.href = `/${url[url.length-1]}`
+	const handleChange = (e) => {
+		setUrl(e.target.value);
+	};
+
+	const join = () => {
+		let tempUrl = "";
+		if (url !== "") {
+			tempUrl = url.split("");
+			window.location.href = `/${tempUrl[tempUrl.lenght - 1]}`;
 		} else {
-			var url = Math.random().toString(36).substring(2, 7)
-			window.location.href = `/${url}`
+			tempUrl = Math.random().toString(36).substring(2, 7);
+			window.location.href = `/${tempUrl}`;
 		}
-	}
+	};
 
-	render() {
-		return (
-			<div className="container2">
-				<div>
-					<h1 style={{ fontSize: "45px" }}>Incogly</h1>
-					<p style={{ fontWeight: "200" }}>We need a new web page</p>
-				</div>
-
-				<div style={{
-					background: "white", width: "30%", height: "auto", padding: "20px", minWidth: "400px",
-					textAlign: "center", margin: "auto", marginTop: "100px"
-				}}>
-					<p style={{ margin: 0, fontWeight: "bold", paddingRight: "50px" }}>Start or join a meeting</p>
-					<Input placeholder="URL" onChange={e => this.handleChange(e)} />
-					<Button variant="contained" color="primary" onClick={this.join} style={{ margin: "20px" }}>Go</Button>
-				</div>
+	return (
+		<div className="container2">
+			<div>
+				<h1 style={{ fontSize: "45px" }}>Incogly</h1>
+				<p style={{ fontWeight: "200" }}>We need a new web page</p>
 			</div>
-		)
-	}
+
+			<div
+				style={{
+					background: "white",
+					width: "30%",
+					height: "auto",
+					padding: "20px",
+					minWidth: "400px",
+					textAlign: "center",
+					margin: "auto",
+					marginTop: "100px",
+				}}
+			>
+				<p
+					style={{
+						margin: 0,
+						fontWeight: "bold",
+						paddingRight: "50px",
+					}}
+				>
+					Start or join a meeting
+				</p>
+				<Input placeholder="URL" onChange={(e) => handleChange(e)} />
+				<Button
+					variant="contained"
+					color="primary"
+					onClick={join}
+					style={{ margin: "20px" }}
+				>
+					Go
+				</Button>
+			</div>
+		</div>
+	);
 }
 
 export default Home;
