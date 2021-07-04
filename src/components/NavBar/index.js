a2cf2725d211b3332b393c7b06bea0445307f210
Raghav Dhingra <raghav.dhingra15@gmail.com>
2021-07-04 17:53:05 +0530

    Remove login/sign up

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,31 +1,10 @@
 import React from "react";
-import { HStack, Spacer, Button } from "@chakra-ui/react";
 import "./index.scss";
 
 function NavBar() {
 	return (
 		<div className="navbar__container">
-			<HStack>
-				<div className=" navbar__nav-item  navbar__heading">
-					Incogly
-				</div>
-				<Spacer />
-				<HStack>
-					<Button
-						variant="contained"
-						className="navbar__nav-item navbar__button navbar__button--dark"
-					>
-						Login
-					</Button>
-					<Spacer />
-					<Button
-						variant="contained"
-						className="navbar__nav-item navbar__button navbar__button--pink"
-					>
-						Sign Up
-					</Button>
-				</HStack>
-			</HStack>
+			<div className=" navbar__nav-item  navbar__heading">Incogly</div>
 		</div>
 	);
 }
