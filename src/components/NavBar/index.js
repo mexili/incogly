8e199cbd5d45650c5c7335467f00ad3094926df5
Raghav Dhingra <raghav.dhingra15@gmail.com>
2021-07-04 18:04:05 +0530

    removed nav links

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,38 +1,10 @@
 import React from "react";
-import { HStack, Center, Spacer, Button } from "@chakra-ui/react";
 import "./index.scss";
 
 function NavBar() {
 	return (
 		<div className="navbar__container">
-			<HStack>
-				<div className=" navbar__nav-item  navbar__heading">
-					Incogly
-				</div>
-				<Spacer />
-				<Center>
-					<div className="navbar__nav-item">Discover</div>
-					<div className="navbar__nav-item">Blog</div>
-					<div className="navbar__nav-item">GitHub</div>
-					<div className="navbar__nav-item">Discord</div>
-				</Center>
-				<Spacer />
-				<HStack>
-					<Button
-						variant="contained"
-						className="navbar__nav-item navbar__button navbar__button--dark"
-					>
-						Login
-					</Button>
-					<Spacer />
-					<Button
-						variant="contained"
-						className="navbar__nav-item navbar__button navbar__button--pink"
-					>
-						Sign Up
-					</Button>
-				</HStack>
-			</HStack>
+			<div className=" navbar__nav-item  navbar__heading">Incogly</div>
 		</div>
 	);
 }
